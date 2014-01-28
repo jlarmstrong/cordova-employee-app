e636b2ad1164f33d08597ab61b2934e15d57802a
Harry Moreno <morenoh149@gmail.com>
2014-01-27 19:34:03 -0800

    finished step 12- geolocation

--- www/js/EmployeeView.js
+++ www/js/EmployeeView.js
@@ -1,6 +1,20 @@
 var EmployeeView = function(adapter, template, employee) {
   this.initialize = function() {
     this.el = $('<div/>');
+    this.el.on('click', '.add-location-btn', this.addLocation);
+  };
+
+  this.addLocation = function(event) {
+    event.preventDefault();
+    navigator.geolocation.getCurrentPosition(
+      function(position) {
+        // in a real app you would store this info
+        alert(position.coords.latitude + ',' + position.coords.longitude);
+      },
+      function() {
+        alert('Error getting location');
+      });
+    return false;
   };
 
   this.render = function() {
