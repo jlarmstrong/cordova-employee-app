3fbc4c2c05833b115ae2f1ad81a101b4170ba5e0
Harry Moreno <morenoh149@gmail.com>
2014-01-27 19:21:05 -0800

    finished tutorial through part 11

---
+++ www/js/EmployeeView.js
@@ -0,0 +1,12 @@
+var EmployeeView = function(adapter, template, employee) {
+  this.initialize = function() {
+    this.el = $('<div/>');
+  };
+
+  this.render = function() {
+    this.el.html(template(employee));
+    return this;
+  };
+
+  this.initialize();
+}
--- www/js/app.js
+++ www/js/app.js
@@ -2,43 +2,47 @@
 (function () {
 
     /* ---------------------------------- Local Variables ---------------------------------- */
+    var homeTpl = Handlebars.compile($("#home-tpl").html());
+    var employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
+    var employeeTpl = Handlebars.compile($("#employee-tpl").html());
+    var detailsURL = /^#employees\/(\d{1,})/;
     var adapter = new MemoryAdapter();
     adapter.initialize().done(function () {
-        console.log("Data adapter initialized");
+      route();
     });
 
     /* --------------------------------- Event Registration -------------------------------- */
-    $('.search-key').on('keyup', findByName);
-    $('.help-btn').on('click', function() {
-        alert("Some help here...")
-    });
-    document.addEventListener('deviceready', function() {
+    document.addEventListener('deviceready', function () {
 
-      FastClick.attach(document.body);
+        FastClick.attach(document.body);
 
-      if (navigator.notification) { // Override default HTML alert with native
-        window.alert = function (message) {
-          navigator.notification.alert(
-            message,    //message
-            null,       //callback
-            "Workshop", //title
-            'OK'        //buttonName
-          );
-        };
-      }
+        if (navigator.notification) { // Override default HTML alert with native dialog
+            window.alert = function (message) {
+                navigator.notification.alert(
+                    message,    // message
+                    null,       // callback
+                    "Workshop", // title
+                    'OK'        // buttonName
+                );
+            };
+        }
     }, false);
 
+    $(window).on('hashchange', route);
+
     /* ---------------------------------- Local Functions ---------------------------------- */
-    function findByName() {
-        adapter.findByName($('.search-key').val()).done(function (employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i = 0; i < l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
+    function route() {
+      var hash = window.location.hash;
+      if (!hash) {
+        $('body').html(new HomeView(adapter, homeTpl, employeeLiTpl).render().el);
+        return;
+      }
+      var match = hash.match(detailsURL);
+      if (match) {
+        adapter.findById(Number(match[1])).done(function(employee) {
+          $('body').html(new EmployeeView(adapter, employeeTpl, employee).render().el);
         });
+      }
     }
 
 }());
