aadfc5892d8682c70af92b04f911c9ecdc0767c6
Harry Moreno <morenoh149@gmail.com>
2014-01-28 17:36:49 -0800

    finished step 14

--- www/js/EmployeeView.js
+++ www/js/EmployeeView.js
@@ -3,7 +3,32 @@ var EmployeeView = function(adapter, template, employee) {
     this.el = $('<div/>');
     this.el.on('click', '.add-location-btn', this.addLocation);
     this.el.on('click', '.add-contact-btn', this.addToContacts);
+    this.el.on('click', '.change-pic-btn', this.changePicture);
   };
+
+  this.changePicture = function(event) {
+    event.preventDefault();
+    if (!navigator.camera) {
+        alert("Camera API not supported", "Error");
+        return;
+    }
+    var options =   {   quality: 50,
+                        destinationType: Camera.DestinationType.DATA_URL,
+                        sourceType: 1,      // 0:Photo Library, 1=Camera, 2=Saved Album
+                        encodingType: 0     // 0=JPG 1=PNG
+                    };
+ 
+    navigator.camera.getPicture(
+        function(imageData) {
+            $('.employee-image', this.el).attr('src', "data:image/jpeg;base64," + imageData);
+        },
+        function() {
+            alert('Error taking picture', 'Error');
+        },
+        options);
+ 
+    return false;
+};
 
   this.addLocation = function(event) {
     event.preventDefault();
