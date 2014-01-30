7b3646004adc0fd3e85be424df12d8372a4ab15c
Harry Moreno <morenoh149@gmail.com>
2014-01-29 22:22:50 -0800

    finished tutorial

--- www/js/EmployeeView.js
+++ www/js/EmployeeView.js
@@ -9,26 +9,25 @@ var EmployeeView = function(adapter, template, employee) {
   this.changePicture = function(event) {
     event.preventDefault();
     if (!navigator.camera) {
-        alert("Camera API not supported", "Error");
-        return;
+      alert("Camera API not supported", "Error");
+      return;
     }
-    var options =   {   quality: 50,
-                        destinationType: Camera.DestinationType.DATA_URL,
-                        sourceType: 1,      // 0:Photo Library, 1=Camera, 2=Saved Album
-                        encodingType: 0     // 0=JPG 1=PNG
-                    };
- 
+    var options = { quality: 50,
+                    destinationType: Camera.DestinationType.DATA_URL,
+                    sourceType: 1,  // 0:Photo Library, 1=Camera, 2=Saved Album
+                    encodingType: 0 // 0=JPG 1=PNG
+    };
     navigator.camera.getPicture(
-        function(imageData) {
-            $('.employee-image', this.el).attr('src', "data:image/jpeg;base64," + imageData);
-        },
-        function() {
-            alert('Error taking picture', 'Error');
-        },
-        options);
- 
+      function(imageData) {
+        $('.employee-image', this.el).attr('src', "data:image/jpeg;base64," + imageData);
+      },
+      function() {
+        alert('Error taking picture', 'Error');
+      },
+      options);
+
     return false;
-};
+  };
 
   this.addLocation = function(event) {
     event.preventDefault();
--- www/js/app.js
+++ www/js/app.js
@@ -6,6 +6,7 @@
     var employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
     var employeeTpl = Handlebars.compile($("#employee-tpl").html());
     var detailsURL = /^#employees\/(\d{1,})/;
+    var slider = new PageSlider($('body'));
     var adapter = new MemoryAdapter();
     adapter.initialize().done(function () {
       route();
@@ -34,13 +35,13 @@
     function route() {
       var hash = window.location.hash;
       if (!hash) {
-        $('body').html(new HomeView(adapter, homeTpl, employeeLiTpl).render().el);
+        slider.slidePage(new HomeView(adapter, homeTpl, employeeLiTpl).render().el);
         return;
       }
       var match = hash.match(detailsURL);
       if (match) {
         adapter.findById(Number(match[1])).done(function(employee) {
-          $('body').html(new EmployeeView(adapter, employeeTpl, employee).render().el);
+          slider.slidePage(new EmployeeView(adapter, employeeTpl, employee).render().el);
         });
       }
     }
